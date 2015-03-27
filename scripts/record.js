dd113d23c8f5003ef11dd2e4833902b6f6603550
Łukasz Polowczyk <look997@gmail.com>
2015-03-27 19:34:12 +0100

    asff

--- scripts/record.js
+++ scripts/record.js
@@ -102,8 +102,7 @@ var onSuccess = function(stream) {
 
 		}
 	};
-	document.removeEventListener("keydown", playStopRecord);
-	document.addEventListener("keydown", playStopRecord);
+	document.onkeydown = playStopRecord;
 };
 
 
